50304df650743e2050817b9ec3d8651244cca0cb
Matthew Somerville <matthew@mysociety.org>
2012-11-16 15:44:30 +0000

    Speech UpdateView.

--- speeches/urls.py
+++ speeches/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import patterns, url
 
-from speeches.views import SpeechCreate, SpeechView, SpeechList
+from speeches.views import SpeechCreate, SpeechUpdate, SpeechView, SpeechList
 
 urlpatterns = patterns('',
     url(r'^$', SpeechList.as_view(), name='speech-list'),
     url(r'^/add$', SpeechCreate.as_view(), name='speech-add'),
     url(r'^/(?P<pk>\d+)$', SpeechView.as_view(), name='speech-view'),
+    url(r'^/(?P<pk>\d+)/edit$', SpeechUpdate.as_view(), name='speech-update'),
 )
 
