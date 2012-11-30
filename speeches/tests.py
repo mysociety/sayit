f7c3a1a9e01a80019915c93a534b7b486d8ada41
Steve Day <steve@mysociety.org>
2012-11-30 17:49:46 +0000

    WIP - adding tests for transcription task and population command

--- speeches/tests.py
+++ speeches/tests.py
@@ -5,12 +5,19 @@ Testing documentation is at https://docs.djangoproject.com/en/1.4/topics/testing
 
 import os
 
+from mock import patch
+
 from selenium import webdriver
 
+from requests import Response
+
 from django.test import TestCase, LiveServerTestCase
+from django.core.files import File
+from django.conf import settings
 
 import speeches
 from speeches.models import Speech, Speaker
+from speeches.tasks import transcribe_speech
 
 class SpeechTest(TestCase):
 
@@ -129,6 +136,111 @@ class SpeechTest(TestCase):
         self.assertFalse('Please wait' in resp.content)        
         self.assertTrue(text in resp.content)
 
+class TranscribeTaskTests(TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls._speeches_path = os.path.abspath(speeches.__path__[0])
+        # Tell Celery to be "eager", ie: run tasks straight away
+        # as if they were normal methods
+        if hasattr(settings, 'CELERY_ALWAYS_EAGER'):
+            cls._OLD_CELERY_ALWAYS_EAGER = settings.CELERY_ALWAYS_EAGER
+        settings.CELERY_ALWAYS_EAGER = True
+
+    @classmethod
+    def tearDownClass(cls):
+        if hasattr(cls, '_OLD_CELERY_ALWAYS_EAGER'):
+            settings.CELERY_ALWAYS_EAGER = cls._OLD_CELERY_ALWAYS_EAGER
+
+    def setUp(self):
+        # Put a speech in the db for the task to use
+        audio = open(os.path.join(self._speeches_path, 'fixtures', 'lamb.mp3'), 'rb')
+        self.speech = Speech.objects.create(audio=File(audio, "lamb.mp3"))
+
+    def tearDown(self):
+        self.speech.delete()
+
+    @patch('requests.post')
+    def test_happy_path(self, patched_post):
+        # Test data
+        auth_response = Response(
+            status_code=200,
+            json={
+                'access_token': 'bb2da510a68542df9e4051cd9ebb0a5a',
+                'expires_in': '0',
+                'refresh_token': 'a096a765c27ff1ef7a0379d387769d603e6ad6c0'
+            }
+        )
+        transcription = 'A transcription'
+        transcription_response = Response(
+            status_code=200,
+            json={
+                'Recognition': {
+                    'Status': 'OK',
+                    'ResponseId': 'c77727642111312f5cce0115a2ba8ce4',
+                    'NBest': [
+                        {   
+                            'WordScores': [0.07, 0.1],
+                            'Confidence': 0.601629956,
+                            'Grade': 'accept',
+                            'ResultText': transcription,
+                            'Words': transcription.split(),
+                            'LanguageId': 'en-US',
+                            'Hypothesis': transcription
+                        }
+                    ]
+                }
+            }
+        )
+
+        # Setup the return values for our patched post method
+        def return_side_effect(*args, **kwargs):
+            if(args[0] == settings.ATT_OAUTH_URL):
+                return auth_response
+            elif(args[0] == settings.ATT_API_URL):
+                return transcription_response        
+        
+        # Call our task to transcribe our file
+        with patch(requests, 'post') as patched_post:
+            patched_post.side_effect = return_side_effect
+            result = transcribe_speech(self.speech.id)
+
+        self.assertTrue(result.successful())
+
+        # Assert that it saved the right data into the db
+        self.assertTrue(transcription in speech.text)
+        self.assertTrue(speech.celery_task_id is None)
+
+    def test_speech_validation(self):
+        self.fail()
+
+    def test_wav_file_creation(self):
+        self.fail()
+
+    def test_always_deletes_tmp_file(self):
+        self.fail()
+
+    def test_auth_retrieval(self):
+        self.fail()
+
+    def test_api_call(self):
+        self.fail()
+
+    def test_transcription_selection(self):
+        self.fail()
+
+    def test_always_clears_task_on_error(self):
+        self.fail()
+
+class PopulateSpeakerCommandTests(TestCase):
+
+    def test_populates_empty_db(self):
+        self.fail()
+
+    def test_updates_existing_records(self):
+        self.fail()
+
+
 class SeleniumTests(LiveServerTestCase):
     @classmethod
     def setUpClass(cls):
@@ -146,4 +258,8 @@ class SeleniumTests(LiveServerTestCase):
         text_input.send_keys('This is a speech')
         self.selenium.find_element_by_xpath('//input[@value="Add speech"]').click()
         self.assertIn('/speech/1', self.selenium.current_url)
+
+    # TODO - test the ajax uploading
+
+    # TODO - test the ajax autocomplete
 
