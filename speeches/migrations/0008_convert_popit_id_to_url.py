fa621bb724dbf33f81b2dfab43c1f406e8aba17e
Steve Day <steve@mysociety.org>
2012-12-20 15:20:10 +0000

    Turn stored popit ids into urls

---
+++ speeches/migrations/0008_convert_popit_id_to_url.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import DataMigration
+from django.db import models
+
+class Migration(DataMigration):
+
+    def forwards(self, orm):
+        from django.conf import settings
+        # Turn all ids into urls, in preparation for renaming popit_id
+        # popit_url
+        for speaker in orm['speeches.Speaker'].objects.all():
+            popit_id = speaker.popit_id;
+            popit_url = '{0}.{1}/api/{2}/person/{3}'.format(settings.POPIT_INSTANCE, 
+                settings.POPIT_HOSTNAME,
+                settings.POPIT_API_VERSION, 
+                popit_id)
+            speaker.popit_id = popit_url
+            speaker.save()
+
+    def backwards(self, orm):
+        from django.conf import settings
+        # Turn all ids into urls, in preparation for renaming popit_id
+        # popit_url
+        for speaker in orm['speeches.Speaker'].objects.all():
+            popit_url = speaker.popit_id;
+            popit_url_start = "{0}.{1}/api/{2}/person/".format(settings.POPIT_INSTANCE, 
+                settings.POPIT_HOSTNAME,
+                settings.POPIT_API_VERSION)
+            popit_id = popit_url.replace(popit_url_start, '')
+            speaker.popit_id = popit_id
+            speaker.save()
+
+    models = {
+        'speeches.speaker': {
+            'Meta': {'object_name': 'Speaker'},
+            'created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'modified': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'name': ('django.db.models.fields.TextField', [], {'db_index': 'True'}),
+            'popit_id': ('django.db.models.fields.TextField', [], {'unique': 'True'})
+        },
+        'speeches.speech': {
+            'Meta': {'object_name': 'Speech'},
+            'audio': ('django.db.models.fields.files.FileField', [], {'max_length': '255', 'blank': 'True'}),
+            'celery_task_id': ('django.db.models.fields.CharField', [], {'max_length': '256', 'null': 'True', 'blank': 'True'}),
+            'created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'end': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+            'event': ('django.db.models.fields.TextField', [], {'db_index': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'location': ('django.db.models.fields.TextField', [], {'db_index': 'True', 'blank': 'True'}),
+            'modified': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'source_url': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'speaker': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['speeches.Speaker']", 'null': 'True', 'on_delete': 'models.SET_NULL', 'blank': 'True'}),
+            'start': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+            'text': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'title': ('django.db.models.fields.TextField', [], {'blank': 'True'})
+        }
+    }
+
+    complete_apps = ['speeches']
+    symmetrical = True
