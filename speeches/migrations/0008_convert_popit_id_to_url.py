73609a24500c6c58af18be992a10943140cf21f1
Steve Day <steve@mysociety.org>
2012-12-20 16:29:45 +0000

    Add http:// to popit urls in data migration

--- speeches/migrations/0008_convert_popit_id_to_url.py
+++ speeches/migrations/0008_convert_popit_id_to_url.py
@@ -12,7 +12,7 @@ class Migration(DataMigration):
         # popit_url
         for speaker in orm['speeches.Speaker'].objects.all():
             popit_id = speaker.popit_id;
-            popit_url = '{0}.{1}/api/{2}/person/{3}'.format(settings.POPIT_INSTANCE, 
+            popit_url = 'http://{0}.{1}/api/{2}/person/{3}'.format(settings.POPIT_INSTANCE, 
                 settings.POPIT_HOSTNAME,
                 settings.POPIT_API_VERSION, 
                 popit_id)
@@ -25,7 +25,7 @@ class Migration(DataMigration):
         # popit_url
         for speaker in orm['speeches.Speaker'].objects.all():
             popit_url = speaker.popit_id;
-            popit_url_start = "{0}.{1}/api/{2}/person/".format(settings.POPIT_INSTANCE, 
+            popit_url_start = 'http://{0}.{1}/api/{2}/person/'.format(settings.POPIT_INSTANCE, 
                 settings.POPIT_HOSTNAME,
                 settings.POPIT_API_VERSION)
             popit_id = popit_url.replace(popit_url_start, '')
