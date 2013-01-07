e1514fa5bcc35df74295c254df65e8e99dc289a1
Steve Day <steve@mysociety.org>
2013-01-07 16:18:33 +0000

    Remove BootstrapSplitDateTimeWidget as it's no longer needed

--- speeches/util.py
+++ speeches/util.py
@@ -1,5 +1,4 @@
 from speeches.tasks import transcribe_speech
-from django.forms.widgets import SplitDateTimeWidget
 
 """Common utility functions/classes
 Things that are needed by multiple bits of code but are specific enough to
@@ -18,27 +17,3 @@ def start_transcribing_speech(speech):
         # Finally, we can remember the new task in the model
         speech.celery_task_id = result.task_id
         speech.save()
-
-class BootstrapSplitDateTimeWidget(SplitDateTimeWidget):
-    """
-    A Widget that splits datetime input into two <input type="text"> boxes and styles with Bootstrap
-    """
-
-    def __init__(self, attrs=None, date_format=None, time_format=None):
-        super(BootstrapSplitDateTimeWidget, self).__init__(attrs, date_format, time_format)
-
-    def format_output(self, rendered_widgets):
-        """Override the output formatting to return widgets with some Bootstrap niceness"""
-        
-        output = ''
-        
-        for i, widget in enumerate(rendered_widgets):
-            output += '<div class="input-append">'
-            output += widget
-            if i == 0:
-                output += '<span class="add-on"><i class="icon-calendar"></i></span>'
-            else:
-                output += '<span class="add-on"><i class="icon-time"></i></span>'
-            output += '</div>'
-
-        return output
