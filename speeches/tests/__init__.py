e52bcaa8789beccceba4dbc823c1ba98bd6132e6
Steve Day <steve@mysociety.org>
2013-01-10 12:16:51 +0000

    Add tests for speaker pages to match those for meeting and debates, testing lists and buttons

--- speeches/tests/__init__.py
+++ speeches/tests/__init__.py
@@ -6,3 +6,4 @@ from speeches.tests.transcribe_task_tests import *
 from speeches.tests.speech_api_tests import *
 from speeches.tests.meeting_tests import *
 from speeches.tests.debate_tests import *
+from speeches.tests.speaker_tests import *
---
+++ speeches/tests/speaker_tests.py
@@ -0,0 +1,33 @@
+from django.test import TestCase
+
+from speeches.models import Speaker, Speech
+
+class SpeakerTests(TestCase):
+    """Tests for the speaker functionality"""
+
+    def test_speaker_page_lists_speeches(self):
+        # Add a speaker
+        speaker = Speaker.objects.create(popit_url='http://popit.mysociety.org/api/v1/person/abcd', name='Steve')
+        
+        # Call the speaker's page
+        resp = self.client.get('/speaker/1')
+
+        # Assert no speeches
+        self.assertSequenceEqual([], resp.context['speech_list'])
+
+        # Add a speech
+        speech = Speech.objects.create(text="A test speech", speaker=speaker)
+
+        # Call the speaker's page again
+        resp = self.client.get('/speaker/1')
+
+        self.assertSequenceEqual([speech], resp.context['speech_list'])
+
+    def test_speaker_page_has_button_to_add_speech(self):
+        # Add a speaker
+        speaker = Speaker.objects.create(popit_url='http://popit.mysociety.org/api/v1/person/abcd', name='Steve')
+        
+        # Call the speaker's page
+        resp = self.client.get('/speaker/1')
+
+        self.assertContains(resp, '<a class="btn" href="/speech/add?speaker=1">Add a new speech by Steve</a>', html=True)
