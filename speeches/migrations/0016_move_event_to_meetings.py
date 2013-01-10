15ad3c8cc736b2b89756836a1f9b253cbfda39dc
Steve Day <steve@mysociety.org>
2013-01-10 17:34:13 +0000

    Add data migration to translate current event field into meetings and debates

---
+++ speeches/migrations/0016_move_event_to_meetings.py
@@ -0,0 +1,83 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import DataMigration
+from django.db import models
+
+class Migration(DataMigration):
+
+    def forwards(self, orm):
+        for speech in orm['speeches.Speech'].objects.all():
+            # Take the event field and create a meeting and debate for it
+            if speech.event:
+                meeting = orm['speeches.Meeting'].objects.create(title=speech.event, date=speech.start_date)
+                debate = orm['speeches.Debate'].objects.create(title=speech.event, meeting=meeting)
+                speech.debate = debate
+                speech.save()
+
+    def backwards(self, orm):
+        for speech in orm['speeches.Speech'].objects.all():
+            # Put data from a related debate into the event field
+            if speech.debate:
+                speech.event = speech.debate.title
+                speech.debate = None
+                speech.save()
+
+        # Delete debates which aren't referenced from any speech
+        for debate in orm['speeches.Debate'].objects.all():
+            if not orm['speeches.Speech'].objects.all().filter(debate=debate):
+                debate.delete()
+
+        # Delete meetings which aren't referenced from any debate
+        for meeting in orm['speeches.Meeting'].objects.all():
+            if not orm['speeches.Debate'].objects.all().filter(meeting=meeting):
+                meeting.delete();
+
+    models = {
+        'speeches.debate': {
+            'Meta': {'object_name': 'Debate'},
+            'created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'meeting': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['speeches.Meeting']", 'null': 'True', 'blank': 'True'}),
+            'modified': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '255'})
+        },
+        'speeches.meeting': {
+            'Meta': {'object_name': 'Meeting'},
+            'created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'date': ('django.db.models.fields.DateField', [], {'null': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'modified': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '255'})
+        },
+        'speeches.speaker': {
+            'Meta': {'object_name': 'Speaker'},
+            'created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'modified': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'name': ('django.db.models.fields.TextField', [], {'db_index': 'True'}),
+            'popit_url': ('django.db.models.fields.TextField', [], {'unique': 'True'})
+        },
+        'speeches.speech': {
+            'Meta': {'object_name': 'Speech'},
+            'audio': ('django.db.models.fields.files.FileField', [], {'max_length': '255', 'blank': 'True'}),
+            'celery_task_id': ('django.db.models.fields.CharField', [], {'max_length': '256', 'null': 'True', 'blank': 'True'}),
+            'created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'debate': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['speeches.Debate']", 'null': 'True', 'blank': 'True'}),
+            'end_date': ('django.db.models.fields.DateField', [], {'null': 'True', 'blank': 'True'}),
+            'end_time': ('django.db.models.fields.TimeField', [], {'null': 'True', 'blank': 'True'}),
+            'event': ('django.db.models.fields.TextField', [], {'db_index': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'location': ('django.db.models.fields.TextField', [], {'db_index': 'True', 'blank': 'True'}),
+            'modified': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'source_url': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'speaker': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['speeches.Speaker']", 'null': 'True', 'on_delete': 'models.SET_NULL', 'blank': 'True'}),
+            'start_date': ('django.db.models.fields.DateField', [], {'null': 'True', 'blank': 'True'}),
+            'start_time': ('django.db.models.fields.TimeField', [], {'null': 'True', 'blank': 'True'}),
+            'text': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'title': ('django.db.models.fields.TextField', [], {'blank': 'True'})
+        }
+    }
+
+    complete_apps = ['speeches']
+    symmetrical = True
