88d81f40ce44d3c8f96cd52d14fe5fa03a2cd775
Steve Day <steve@mysociety.org>
2013-01-21 12:10:44 +0000

    Tidy up arrangement of urls

--- speeches/urls.py
+++ speeches/urls.py
@@ -10,8 +10,9 @@ urlpatterns = patterns('',
     url(r'^speech/ajax_audio$', SpeechAudioCreate.as_view(), name='speech-ajax-audio'),
     url(r'^speech/(?P<pk>\d+)$', SpeechView.as_view(), name='speech-view'),
     url(r'^speech/(?P<pk>\d+)/edit$', SpeechUpdate.as_view(), name='speech-edit'),
-    url(r'^speaker/(?P<pk>\d+)$', SpeakerView.as_view(), name='speaker-view'),
     url(r'^api/v0.1/speech/$', csrf_exempt(SpeechAPICreate.as_view()), name='speech-api-add'),
+
+    url(r'^speaker/(?P<pk>\d+)$', SpeakerView.as_view(), name='speaker-view'),
 
     url(r'^meeting/(?P<pk>\d+)$', MeetingView.as_view(), name='meeting-view'),
     url(r'^meeting/add$', MeetingCreate.as_view(), name='meeting-add'),
