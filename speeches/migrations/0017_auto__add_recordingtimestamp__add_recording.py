17f7ce8b22abea2f031fd17256f7f1b409f85900
Steve Day <steve@mysociety.org>
2013-01-21 13:53:18 +0000

    Add migration for new recording models

---
+++ speeches/migrations/0017_auto__add_recordingtimestamp__add_recording.py
@@ -0,0 +1,112 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding model 'RecordingTimestamp'
+        db.create_table('speeches_recordingtimestamp', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('created', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
+            ('modified', self.gf('django.db.models.fields.DateTimeField')(auto_now=True, blank=True)),
+            ('speaker', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['speeches.Speaker'], null=True, on_delete=models.SET_NULL, blank=True)),
+            ('timestamp', self.gf('django.db.models.fields.DateTimeField')(db_index=True)),
+        ))
+        db.send_create_signal('speeches', ['RecordingTimestamp'])
+
+        # Adding model 'Recording'
+        db.create_table('speeches_recording', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('created', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
+            ('modified', self.gf('django.db.models.fields.DateTimeField')(auto_now=True, blank=True)),
+            ('audio', self.gf('django.db.models.fields.files.FileField')(max_length=255, blank=True)),
+        ))
+        db.send_create_signal('speeches', ['Recording'])
+
+        # Adding M2M table for field timestamps on 'Recording'
+        db.create_table('speeches_recording_timestamps', (
+            ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True)),
+            ('recording', models.ForeignKey(orm['speeches.recording'], null=False)),
+            ('recordingtimestamp', models.ForeignKey(orm['speeches.recordingtimestamp'], null=False))
+        ))
+        db.create_unique('speeches_recording_timestamps', ['recording_id', 'recordingtimestamp_id'])
+
+
+    def backwards(self, orm):
+        # Deleting model 'RecordingTimestamp'
+        db.delete_table('speeches_recordingtimestamp')
+
+        # Deleting model 'Recording'
+        db.delete_table('speeches_recording')
+
+        # Removing M2M table for field timestamps on 'Recording'
+        db.delete_table('speeches_recording_timestamps')
+
+
+    models = {
+        'speeches.debate': {
+            'Meta': {'object_name': 'Debate'},
+            'created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'meeting': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['speeches.Meeting']", 'null': 'True', 'blank': 'True'}),
+            'modified': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '255'})
+        },
+        'speeches.meeting': {
+            'Meta': {'object_name': 'Meeting'},
+            'created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'date': ('django.db.models.fields.DateField', [], {'null': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'modified': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '255'})
+        },
+        'speeches.recording': {
+            'Meta': {'object_name': 'Recording'},
+            'audio': ('django.db.models.fields.files.FileField', [], {'max_length': '255', 'blank': 'True'}),
+            'created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'modified': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'timestamps': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'to': "orm['speeches.RecordingTimestamp']", 'null': 'True', 'blank': 'True'})
+        },
+        'speeches.recordingtimestamp': {
+            'Meta': {'object_name': 'RecordingTimestamp'},
+            'created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'modified': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'speaker': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['speeches.Speaker']", 'null': 'True', 'on_delete': 'models.SET_NULL', 'blank': 'True'}),
+            'timestamp': ('django.db.models.fields.DateTimeField', [], {'db_index': 'True'})
+        },
+        'speeches.speaker': {
+            'Meta': {'object_name': 'Speaker'},
+            'created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'modified': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'name': ('django.db.models.fields.TextField', [], {'db_index': 'True'}),
+            'popit_url': ('django.db.models.fields.TextField', [], {'unique': 'True'})
+        },
+        'speeches.speech': {
+            'Meta': {'object_name': 'Speech'},
+            'audio': ('django.db.models.fields.files.FileField', [], {'max_length': '255', 'blank': 'True'}),
+            'celery_task_id': ('django.db.models.fields.CharField', [], {'max_length': '256', 'null': 'True', 'blank': 'True'}),
+            'created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'debate': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['speeches.Debate']", 'null': 'True', 'blank': 'True'}),
+            'end_date': ('django.db.models.fields.DateField', [], {'null': 'True', 'blank': 'True'}),
+            'end_time': ('django.db.models.fields.TimeField', [], {'null': 'True', 'blank': 'True'}),
+            'event': ('django.db.models.fields.TextField', [], {'db_index': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'location': ('django.db.models.fields.TextField', [], {'db_index': 'True', 'blank': 'True'}),
+            'modified': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'source_url': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'speaker': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['speeches.Speaker']", 'null': 'True', 'on_delete': 'models.SET_NULL', 'blank': 'True'}),
+            'start_date': ('django.db.models.fields.DateField', [], {'null': 'True', 'blank': 'True'}),
+            'start_time': ('django.db.models.fields.TimeField', [], {'null': 'True', 'blank': 'True'}),
+            'text': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'title': ('django.db.models.fields.TextField', [], {'blank': 'True'})
+        }
+    }
+
+    complete_apps = ['speeches']
