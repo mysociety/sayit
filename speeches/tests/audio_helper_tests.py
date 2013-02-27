7f2156e93d58bed33a28134b6253863062f9fbea
Mark Longair <mhl@pobox.com>
2013-02-27 16:21:58 +0000

    Factor out the code to create an expected output filename

--- speeches/tests/audio_helper_tests.py
+++ speeches/tests/audio_helper_tests.py
@@ -26,48 +26,53 @@ class AudioHelperTests(TestCase):
         if self.tmp_filename is not None:
             os.remove(self.tmp_filename)
 
+    def expected_output_file(self, filename):
+        _, extension = os.path.splitext(filename)
+        return os.path.join(self._expected_fixtures,
+                            extension[1:],
+                            filename)
 
     def test_wav_file_creation_from_mp3(self):
         self.tmp_filename = self.helper.make_wav(os.path.join(self._in_fixtures, 'lamb.mp3'))
 
         # Compare the created file to one we made earlier
-        self.assertTrue(filecmp.cmp(self.tmp_filename, os.path.join(self._expected_fixtures, 'wav', 'lamb_from_mp3.wav')))
+        self.assertTrue(filecmp.cmp(self.tmp_filename, self.expected_output_file('lamb_from_mp3.wav')))
 
     def test_wav_file_creation_from_android_3gp(self):
         self.tmp_filename = self.helper.make_wav(os.path.join(self._in_fixtures, 'lamb.3gp'))
 
         # Compare the created file to one we made earlier
-        self.assertTrue(filecmp.cmp(self.tmp_filename, os.path.join(self._expected_fixtures, 'wav', 'lamb_from_3gp.wav')))
+        self.assertTrue(filecmp.cmp(self.tmp_filename, self.expected_output_file('lamb_from_3gp.wav')))
 
     def test_wav_file_creation_from_iphone_wav(self):
         self.tmp_filename = self.helper.make_wav(os.path.join(self._in_fixtures, 'lamb_iphone.wav'))
 
         # Compare the created file to one we made earlier
-        self.assertTrue(filecmp.cmp(self.tmp_filename, os.path.join(self._expected_fixtures, 'wav', 'lamb_from_iphone.wav')))
+        self.assertTrue(filecmp.cmp(self.tmp_filename, self.expected_output_file('lamb_from_iphone.wav')))
 
     def test_wav_file_creation_from_stereo_wav(self):
         self.tmp_filename = self.helper.make_wav(os.path.join(self._in_fixtures, 'lamb_stereo.wav'))
 
         # Compare the created file to one we made earlier
-        self.assertTrue(filecmp.cmp(self.tmp_filename, os.path.join(self._expected_fixtures, 'wav', 'lamb_from_stereo.wav')))
+        self.assertTrue(filecmp.cmp(self.tmp_filename, self.expected_output_file('lamb_from_stereo.wav')))
 
     def test_mp3_file_creation_from_stereo_wav(self):
         self.tmp_filename = self.helper.make_mp3(os.path.join(self._in_fixtures, 'lamb_stereo.wav'))
 
         # Compare the created file to one we made earlier
-        self.assertTrue(filecmp.cmp(self.tmp_filename, os.path.join(self._expected_fixtures, 'mp3', 'lamb_mp3_from_stereo.mp3')))
+        self.assertTrue(filecmp.cmp(self.tmp_filename, self.expected_output_file('lamb_mp3_from_stereo.mp3')))
 
     def test_mp3_file_creation_from_iphone_wav(self):
         self.tmp_filename = self.helper.make_mp3(os.path.join(self._in_fixtures, 'lamb_iphone.wav'))
 
         # Compare the created file to one we made earlier
-        self.assertTrue(filecmp.cmp(self.tmp_filename, os.path.join(self._expected_fixtures, 'mp3', 'lamb_mp3_from_iphone.mp3')))
+        self.assertTrue(filecmp.cmp(self.tmp_filename, self.expected_output_file('lamb_mp3_from_iphone.mp3')))
 
     def test_mp3_file_creation_from_android_3gp(self):
         self.tmp_filename = self.helper.make_mp3(os.path.join(self._in_fixtures, 'lamb.3gp'))
 
         # Compare the created file to one we made earlier
-        self.assertTrue(filecmp.cmp(self.tmp_filename, os.path.join(self._expected_fixtures, 'mp3', 'lamb_mp3_from_3gp.mp3')))
+        self.assertTrue(filecmp.cmp(self.tmp_filename, self.expected_output_file('lamb_mp3_from_3gp.mp3')))
 
     def test_recording_splitting_no_timestamps(self):
         audio = open(os.path.join(self._in_fixtures, 'lamb.mp3'), 'rb')
@@ -76,7 +81,7 @@ class AudioHelperTests(TestCase):
         files_created = self.helper.split_recording(recording)
 
         self.assertEquals(len(files_created), 1)
-        self.assertTrue(filecmp.cmp(files_created[0], os.path.join(self._expected_fixtures, 'mp3', 'lamb_whole.mp3')))
+        self.assertTrue(filecmp.cmp(files_created[0], self.expected_output_file('lamb_whole.mp3')))
 
     def test_recording_splitting_one_timestamp(self):
         speaker = Speaker.objects.create(popit_url='http://popit.mysociety.org/api/v1/person/abcd', name='Steve')
@@ -89,7 +94,7 @@ class AudioHelperTests(TestCase):
         files_created = self.helper.split_recording(recording)
 
         self.assertEquals(len(files_created), 1)
-        self.assertTrue(filecmp.cmp(files_created[0], os.path.join(self._expected_fixtures, 'mp3', 'lamb_whole.mp3')))
+        self.assertTrue(filecmp.cmp(files_created[0], self.expected_output_file('lamb_whole.mp3')))
 
     def test_recording_splitting_several_timestamps(self):
         speaker1 = Speaker.objects.create(popit_url='http://popit.mysociety.org/api/v1/person/abcd', name='Steve')
@@ -111,6 +116,6 @@ class AudioHelperTests(TestCase):
         files_created = self.helper.split_recording(recording)
 
         self.assertEquals(len(files_created), 3)
-        self.assertTrue(filecmp.cmp(files_created[0], os.path.join(self._expected_fixtures, 'mp3', 'lamb_first_three_seconds.mp3')))
-        self.assertTrue(filecmp.cmp(files_created[1], os.path.join(self._expected_fixtures, 'mp3', 'lamb_from_three_to_four_seconds.mp3')))
-        self.assertTrue(filecmp.cmp(files_created[2], os.path.join(self._expected_fixtures, 'mp3', 'lamb_from_four_seconds_onwards.mp3')))
+        self.assertTrue(filecmp.cmp(files_created[0], self.expected_output_file('lamb_first_three_seconds.mp3')))
+        self.assertTrue(filecmp.cmp(files_created[1], self.expected_output_file('lamb_from_three_to_four_seconds.mp3')))
+        self.assertTrue(filecmp.cmp(files_created[2], self.expected_output_file('lamb_from_four_seconds_onwards.mp3')))
