3c23441eea9994779faef9499ab1dc6b52cdde60
Matthew Somerville <matthew-github@dracos.co.uk>
2013-03-05 12:29:40 +0000

    Be sure to restrict InstanceFormMixin to the right objects too.

--- instances/views.py
+++ instances/views.py
@@ -1,11 +1,11 @@
-class InstanceFormMixin(object):
+class InstanceViewMixin(object):
+    def get_queryset(self):
+        return self.model.objects.for_instance(self.request.instance)
+
+class InstanceFormMixin(InstanceViewMixin):
     def form_valid(self, form):
         # If not present, fill in the instance from the request
         if not hasattr(form.instance, 'instance'):
             form.instance.instance = self.request.instance
         return super(InstanceFormMixin, self).form_valid(form)
-
-class InstanceViewMixin(object):
-    def get_queryset(self):
-        return self.model.objects.for_instance(self.request.instance)
 
