mysociety/spoke

Initial Django from template.

conf/post_deploy_actions.bash
@@ -1,0 +1,26 @@
+#!/bin/bash
+
+# abort on any errors
+set -e
+
+# check that we are in the expected directory
+cd `dirname $0`/..
+
+# create/update the virtual environment
+# NOTE: some packages are difficult to install if they are not site packages,
+# for example xapian. If using these you might want to add the
+# '--enable-site-packages' argument.
+virtualenv --no-site-packages ../virtualenv-spoke
+source ../virtualenv-spoke/bin/activate
+pip install --requirement requirements.txt
+
+# make sure that there is no old code (the .py files may have been git deleted)
+find . -name '*.pyc' -delete
+
+# get the database up to speed
+./manage.py syncdb
+./manage.py migrate
+
+# gather all the static files in one place
+./manage.py collectstatic --noinput
+

spoke/settings/mysociety.py
@@ -1,0 +1,24 @@
+# load the mySociety config from its special file
+
+import yaml
+from .paths import *
+
+config = yaml.load(open(os.path.join(PROJECT_ROOT, 'conf', 'general.yml')))
+
+DEBUG = bool(int(config.get('STAGING')))
+TEMPLATE_DEBUG = DEBUG
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': config.get('SPOKE_DB_NAME'),
+        'USER': config.get('SPOKE_DB_USER'),
+        'PASSWORD': config.get('SPOKE_DB_PASS'),
+        'HOST': config.get('SPOKE_DB_HOST'),
+        'PORT': config.get('SPOKE_DB_PORT'),
+    }
+}
+
+TIME_ZONE = config.get('TIME_ZONE')
+SECRET_KEY = config.get('DJANGO_SECRET_KEY')
+GOOGLE_ANALYTICS_ACCOUNT = config.get('GOOGLE_ANALYTICS_ACCOUNT')

mysociety/sayit

conf/post_deploy_actions.bash
@@ -1,0 +1,26 @@
+#!/bin/bash
+
+# abort on any errors
+set -e
+
+# check that we are in the expected directory
+cd `dirname $0`/..
+
+# create/update the virtual environment
+# NOTE: some packages are difficult to install if they are not site packages,
+# for example xapian. If using these you might want to add the
+# '--enable-site-packages' argument.
+virtualenv --no-site-packages ../virtualenv-sayit
+source ../virtualenv-sayit/bin/activate
+pip install --requirement requirements.txt
+
+# make sure that there is no old code (the .py files may have been git deleted)
+find . -name '*.pyc' -delete
+
+# get the database up to speed
+./manage.py syncdb
+./manage.py migrate
+
+# gather all the static files in one place
+./manage.py collectstatic --noinput
+

spoke/settings/mysociety.py
@@ -1,0 +1,24 @@
+# load the mySociety config from its special file
+
+import yaml
+from .paths import *
+
+config = yaml.load(open(os.path.join(PROJECT_ROOT, 'conf', 'general.yml')))
+
+DEBUG = bool(int(config.get('STAGING')))
+TEMPLATE_DEBUG = DEBUG
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': config.get('SAYIT_DB_NAME'),
+        'USER': config.get('SAYIT_DB_USER'),
+        'PASSWORD': config.get('SAYIT_DB_PASS'),
+        'HOST': config.get('SAYIT_DB_HOST'),
+        'PORT': config.get('SAYIT_DB_PORT'),
+    }
+}
+
+TIME_ZONE = config.get('TIME_ZONE')
+SECRET_KEY = config.get('DJANGO_SECRET_KEY')
+GOOGLE_ANALYTICS_ACCOUNT = config.get('GOOGLE_ANALYTICS_ACCOUNT')