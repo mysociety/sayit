4cb558341df1b297846fea84523e88a0220c0d05
Matthew Somerville <matthew-github@dracos.co.uk>
2013-04-03 18:34:04 +0100

    Add unsectioned speeches to root section view.

--- speeches/views.py
+++ speeches/views.py
@@ -135,8 +135,12 @@ class SectionList(InstanceViewMixin, ListView):
     model = Section
     context_object_name = 'section_list'
 
-    #def get_queryset(self):
-    #    return super(SectionList, self).get_queryset().order_by("-created")
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super(SectionList, self).get_context_data(**kwargs)
+        # Add in a QuerySet of all the speeches not in a section
+        context['speech_list'] = Speech.objects.for_instance(self.request.instance).filter(section=None)
+        return context
 
 class SectionMixin(InstanceFormMixin):
     model = Section
