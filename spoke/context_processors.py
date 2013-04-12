2e6b3203834b17983bddd31ef1d6235a4d46fb12
Matthew Somerville <matthew-github@dracos.co.uk>
2013-04-12 18:59:25 +0100

    Add link to host from instance navigation.

--- spoke/context_processors.py
+++ spoke/context_processors.py
@@ -2,9 +2,16 @@ from django.conf import settings
 
 def add_settings( request ):
     """Add some selected settings values to the context"""
+
+    url = 'http://' + settings.BASE_HOST
+    s = request.get_host().rsplit(':', 1)
+    if len(s) == 2:
+        url += ':' + s[1]
+
     return {
         'settings': {
             'GOOGLE_ANALYTICS_ACCOUNT': settings.GOOGLE_ANALYTICS_ACCOUNT,
             'DEBUG': settings.DEBUG,
+            'BASE_HOST': url,
         }
     }
