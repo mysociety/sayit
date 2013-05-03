e64836cbf46ed605579afbf3a2bff515e075cd35
Hakim Cassimally <hakim.cassimally@gmail.com>
2013-05-03 18:51:08 +0100

    Test for #28 PopIt person passed correctly to template

--- speeches/tests/speaker_tests.py
+++ speeches/tests/speaker_tests.py
@@ -1,16 +1,26 @@
 from instances.tests import InstanceTestCase
 
 from speeches.models import Speaker, Speech
+from popit.models import Person, ApiInstance
 
 class SpeakerTests(InstanceTestCase):
     """Tests for the speaker functionality"""
 
     def test_speaker_page_lists_speeches(self):
         # Add a speaker
-        speaker = Speaker.objects.create(name='Steve', instance=self.instance)
+
+        api_url = 'http://popit.mysociety.org/api/v1/'
+        ai = ApiInstance.objects.create(url=api_url)
+
+        popit_person = Person.objects.create(name='Steve', summary='A froody dude', image='http://example.com/image.jpg', api_instance=ai)
+        speaker = Speaker.objects.create(name='Steve', instance=self.instance, person=popit_person)
         
         # Call the speaker's page
         resp = self.client.get('/speaker/1')
+
+        self.assertSequenceEqual("Steve", resp.context['speaker'].person.name)
+        self.assertSequenceEqual("A froody dude", resp.context['speaker'].person.summary)
+        self.assertSequenceEqual("http://example.com/image.jpg", resp.context['speaker'].person.image)
 
         # Assert no speeches
         self.assertSequenceEqual([], resp.context['speech_list'])
