86f7ccfc95fef7fc938d586a71a8a29635249ca8
Hakim Cassimally <hakim.cassimally@gmail.com>
2013-06-03 11:09:20 +0100

    Better test data
    using 3 timestamps confirms assumption that timestamp offset is
    relative to the beginning (as opposed to the previous timestamp)

--- speeches/tests/recording_timestamp_tests.py
+++ speeches/tests/recording_timestamp_tests.py
@@ -21,12 +21,12 @@ class RecordingTimestampTests(RecordingAPITests):
     def test_modify_timestamps(self, filename='lamb.mp3'):
         audio = open(os.path.join(self._in_fixtures, filename), 'rb')
 
-        SPEECHES = 2
+        SPEECHES = 3
         # NOTE: this test is loosely cargo-culted from recording_api_tests,
         # however here timestamps are hardcoded for simplicity
         resp = self.client.post('/api/v0.1/recording/', {
             'audio': audio,
-            'timestamps': '[{"timestamp":0},{"timestamp":3000}]'
+            'timestamps': '[{"timestamp":0},{"timestamp":2000},{"timestamp":4000}]'
         })
 
         def check_response(resp, expected_code=201, expected_type='application/json'):
@@ -51,7 +51,7 @@ class RecordingTimestampTests(RecordingAPITests):
                     audio_helper.get_audio_duration(rt.speech.audio.path),
                     d)
 
-        check_audio_durations(recording, [3,2])
+        check_audio_durations(recording, [2,2,1])
 
         resp = self.client.post('/recording/%d/edit' % recording.id, {
             # csrfmiddlewaretoken ???
@@ -65,9 +65,13 @@ class RecordingTimestampTests(RecordingAPITests):
             'timestamps-1-recording': recording.id,
             'timestamps-1-id': timestamps[1].id,
             'timestamps-1-speaker': "",
-            'timestamps-1-timestamp': 1,
+            'timestamps-1-timestamp': 3,
+            'timestamps-2-recording': recording.id,
+            'timestamps-2-id': timestamps[2].id,
+            'timestamps-2-speaker': "",
+            'timestamps-2-timestamp': 4,
         })
 
         recording = check_response(resp, 302, 'text/html; charset=utf-8')
 
-        check_audio_durations(recording, [1,4])
+        check_audio_durations(recording, [3,1,1])
