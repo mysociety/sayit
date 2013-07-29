fb456eff8344177d17b592c8c3e7c544f319f428
Matthew Somerville <matthew-github@dracos.co.uk>
2013-07-29 18:23:06 +0100

    Remove instances app from local code.

--- instances/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-from .models import Instance
-
-class InstanceForm(forms.ModelForm):
-    class Meta:
-        model = Instance
-        exclude = ( 'label', 'users', 'created_by' )
-
