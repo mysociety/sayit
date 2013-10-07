31ff0b3024c57eaae915e2d2feff93ef9fef3975
Hakim Cassimally <hakim@mysociety.org>
2013-10-07 14:46:21 +0000

    Test case for multiple parent_section_titles

--- speeches/tests/import_json_tests.py
+++ speeches/tests/import_json_tests.py
@@ -27,7 +27,7 @@ class ImportJsonTests(InstanceTestCase):
         files = [
                 # filename, speech count, resolved count, section title, section parent titles
                 ('1.txt', 8, 0, "Agriculture, Forestry and Fisheries", ["Top Section", "Middle Section", "Bottom Section"]),
-                #('2.txt', 6, 0),
+                ('2.txt', 6, 0, "Agriculture, Forestry and Fisheries", ["Top Section", "Middle Section", "Other Bottom Section"]),
                 #('3.txt', 8, 0),
                 #('4.txt', 5, 0),
                 #('5.txt', 8, 0),
@@ -37,11 +37,15 @@ class ImportJsonTests(InstanceTestCase):
                 #('9.txt', 7, 0)
                 ]
 
+        sections = []
+
         for (f, exp_speeches, exp_resolved, exp_section_name, exp_section_parents) in files:
             document_path = os.path.join(self._in_fixtures, f)
 
             aj = ImportJson(instance=self.instance, category_field="title", commit=True)
             section = aj.import_document(document_path)
+
+            sections.append(section)
 
             self.assertTrue(section is not None)
 
@@ -63,3 +67,10 @@ class ImportJsonTests(InstanceTestCase):
             self.assertEquals( len(resolved), exp_resolved,
                    'Resolved %d == %d (%s)' %
                    (len(resolved), exp_resolved, f) )
+
+        s0_grandparent = sections[0].parent.parent.parent
+        s1_grandparent = sections[1].parent.parent.parent
+        self.assertEquals( s0_grandparent.title, 'Top Section' )
+        self.assertEquals( s1_grandparent.title, 'Top Section' )
+        self.assertEquals( s0_grandparent.id, s1_grandparent.id )
+
