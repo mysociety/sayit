d633c788b4270cbe4af08b3da630cda4110b89ec
Matthew Somerville <matthew-github@dracos.co.uk>
2013-10-16 17:11:56 +0100

    [Leveson] Remove middle names of speakers.

--- scrapers/leveson/utils.py
+++ scrapers/leveson/utils.py
@@ -2,7 +2,13 @@ import re
 
 def prettify(s):
     s = s.title()
-    s = s.replace('Dac', 'DAC').replace('Qc', 'QC')
+    s = s.replace(' Of ', ' of ').replace('Dac ', 'DAC ') \
+         .replace('Qc', 'QC').replace('Ds ', 'DS ')
     # Deal with the McNames
     s = re.sub('Mc[a-z]', lambda mo: mo.group(0)[:-1] + mo.group(0)[-1].upper(), s)
+    s = s.replace('Maclennan', 'MacLennan')
+    # Remove middle names
+    s = re.sub('^(DAC|DS|Dr|Miss|Mrs|Mr|Ms|Baroness|Lord|Professor|Sir) (\S+ )(?:\S+ )+?(\S+)((?: QC)?)$', r'\1 \2\3\4', s)
+    if s != 'David Allen Green':
+        s = re.sub('^(?!DAC|DS|Dr|Miss|Mrs|Mr|Ms|Baroness|Lord|Professor|Sir)(\S+) \S+ (\S+)', r'\1 \2', s)
     return s
