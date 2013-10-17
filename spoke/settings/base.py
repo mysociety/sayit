e6e7b79c84d404c3c5e80ba11754467fbd54b17b
Matthew Somerville <matthew@mysociety.org>
2013-10-17 14:55:16 +0100

    Use database-based index name for search engine.

--- spoke/settings/base.py
+++ spoke/settings/base.py
@@ -223,7 +223,7 @@ HAYSTACK_CONNECTIONS = {
     'default': {
         'ENGINE': 'haystack.backends.elasticsearch_backend.ElasticsearchSearchEngine',
         'URL': 'http://127.0.0.1:9200/',
-        'INDEX_NAME': 'haystack',
+        'INDEX_NAME': DATABASES['default']['NAME'],
     },
 }
 
