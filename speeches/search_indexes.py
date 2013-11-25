9df566dd4b163fc6364fb91ffb76231a30aaed91
Matthew Somerville <matthew-github@dracos.co.uk>
2013-11-25 17:09:06 +0000

    Supply updated field to search indexes.

--- speeches/search_indexes.py
+++ speeches/search_indexes.py
@@ -17,6 +17,9 @@ class SpeechIndex(indexes.SearchIndex, indexes.Indexable):
         """Used when the entire index for model is updated."""
         return self.get_model().objects # .filter(pub_date__lte=datetime.datetime.now())
 
+    def get_updated_field(self):
+        return 'modified'
+
 class SpeakerIndex(indexes.SearchIndex, indexes.Indexable):
     text = indexes.CharField(document=True, model_attr='name')
     instance = indexes.CharField(model_attr='instance__label')
@@ -24,9 +27,15 @@ class SpeakerIndex(indexes.SearchIndex, indexes.Indexable):
     def get_model(self):
         return Speaker
 
+    def get_updated_field(self):
+        return 'modified'
+
 class SectionIndex(indexes.SearchIndex, indexes.Indexable):
     text = indexes.CharField(document=True, model_attr='title')
     instance = indexes.CharField(model_attr='instance__label')
 
     def get_model(self):
         return Section
+
+    def get_updated_field(self):
+        return 'modified'
