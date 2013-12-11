df6bd57c7cce2272a9ce0f847d7fc6a55f98102a
Matthew Somerville <matthew-github@dracos.co.uk>
2013-12-11 15:08:17 +0000

    Be sure to include line break after stage direction.

--- speeches/management/commands/load_shakespeare.py
+++ speeches/management/commands/load_shakespeare.py
@@ -123,7 +123,8 @@ class Command(BaseCommand):
                         if len(line):
                             text += '<i>%s</i>' % line[0].text
                             if line[0].tail:
-                                text += ' %s\n' % line[0].tail.strip()
+                                text += ' %s' % line[0].tail.strip()
+                            text += '\n'
                         elif line.tag == 'LINE':
                             text += '%s\n' % line.text
                         elif line.tag == 'STAGEDIR':
