39b8fcc842333f7d9da952f8241afb023d75f94b
Matthew Somerville <matthew-github@dracos.co.uk>
2014-01-10 13:56:07 +0000

    Fix error with non-sectioned search result.

--- speeches/tests/search_tests.py
+++ speeches/tests/search_tests.py
@@ -18,3 +18,14 @@ class SearchTests(InstanceTestCase):
         self.assertEqual( results.filter(speaker=s1.id).count(), 2 )
         self.assertEqual( results.filter(speaker=s2.id).count(), 1 )
         self.assertEqual( results.filter(instance=None).count(), 0 )
+
+    def test_search_results(self):
+        s1 = self.instance.speaker_set.create(name='Speaker 1')
+        s2 = self.instance.speaker_set.create(name='Speaker 2')
+        self.instance.speech_set.create(speaker=s1, text='Some text by speaker 1')
+        self.instance.speech_set.create(speaker=s1, text='Some more text by speaker 1')
+        self.instance.speech_set.create(speaker=s2, text='Some text by speaker 2')
+        call_command('rebuild_index', verbosity=0, interactive=False)
+
+        resp = self.client.get('/search/?q=more')
+        self.assertContains(resp, 'Some <em>more</em> text by speaker 1')
