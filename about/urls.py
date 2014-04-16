d09e635e363c36397ca8798ebd7cf66e0e82f47a
Matthew Somerville <matthew-github@dracos.co.uk>
2014-04-16 13:55:37 +0100

    Remove sayit.mysociety.org project files.

--- about/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import patterns, url, include
-
-from about.views import *
-
-urlpatterns = patterns('',
-    (r'^$', AboutView.as_view(), { 'slug': 'index' }),
-    (r'^/(?P<slug>.+)$', AboutView.as_view()),
-)
-
--- spoke/shortcuts.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render_to_response
-from django.template import RequestContext
-
-def render(request, template_name, context=None):
-    if context is None: context = {}
-    return render_to_response(template_name, context,
-        context_instance = RequestContext(request)
-    )
