180e574471d449cfb3500c720741b36008917ec0
Matthew Somerville <matthew-github@dracos.co.uk>
2014-05-06 11:41:47 +0100

    Include admin in example project.

--- example_project/urls.py
+++ example_project/urls.py
@@ -27,6 +27,8 @@ if 'test' in sys.argv:
     )
 
 urlpatterns += patterns('',
+    (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    (r'^admin/', include(admin.site.urls)),
 
     url(r'^accounts/login/$', 'django.contrib.auth.views.login'),
     url(r'^accounts/logout/$', 'django.contrib.auth.views.logout'),
