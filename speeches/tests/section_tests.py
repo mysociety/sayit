f3e9ca136f41c3eebee5c25ba6f52e35b0323c88
Zarino Zappia <mail@zarino.co.uk>
2014-05-22 14:20:48 +0100

    section_detail.html: de-emphasise Edit and Subsection buttons

--- speeches/tests/section_tests.py
+++ speeches/tests/section_tests.py
@@ -219,8 +219,8 @@ class SectionSiteTests(InstanceTestCase):
         resp = self.client.get('/sections/%d' % section.id)
 
         self.assertContains(resp, '<a href="/speech/add?section=%d" class="button small right">Add speech</a>' % section.id, html=True)
-        self.assertContains(resp, '<a href="/sections/add?section=%d" class="button small right">Add subsection</a>' % section.id, html=True)
-        self.assertContains(resp, '<a href="/sections/%d/edit" class="button small right">Edit section</a>' % section.id, html=True)
+        self.assertContains(resp, '<a href="/sections/add?section=%d" class="button secondary small right">Add subsection</a>' % section.id, html=True)
+        self.assertContains(resp, '<a href="/sections/%d/edit" class="button secondary small right">Edit section</a>' % section.id, html=True)
 
 
     def test_section_deletion(self):
