ac0109b038620e5e1cb7bb2b5c95c162dad25be9
Duncan Parkes <duncan@mysociety.org>
2014-08-26 15:57:41 +0100

    A bit of PEP8ification.
    Not getting speeches/forms.py down to 0 flake8 messages though, as
    there are a few things I can't see a nice way to do.

--- speeches/forms.py
+++ speeches/forms.py
@@ -30,14 +30,12 @@ except ImportError:
 
 from django.utils.encoding import force_text
 from django import forms
-from django.forms.forms import BoundField
 from django.forms.models import inlineformset_factory, BaseInlineFormSet
-from django.forms.widgets import Textarea
-from django.forms.util import flatatt
 from django.core.files.uploadedfile import UploadedFile
 
 from speeches.fields import FromStartIntegerField
-from speeches.models import Speech, Speaker, Section, Recording, RecordingTimestamp, Tag
+from speeches.models import (Speech, Speaker, Section,
+                             Recording, RecordingTimestamp, Tag)
 from speeches.widgets import AudioFileInput, DatePickerWidget, TimePickerWidget
 from speeches.utils import GroupedModelChoiceField
 
@@ -47,6 +45,7 @@ logger = logging.getLogger(__name__)
 def verbose_name(model, field):
     return model._meta.get_field(field).verbose_name
 
+
 class CleanAudioMixin(object):
     def clean_audio(self):
         audio = self.cleaned_data['audio']
@@ -54,34 +53,45 @@ class CleanAudioMixin(object):
             ext = os.path.splitext(audio.name)[1]
             # Check that the file is an audio file and one of the
             # filetypes we can use
-            if audio.content_type[0:6] != 'audio/' or ext not in ('.ogg', '.mp3', '.wav', '.3gp'):
-                raise forms.ValidationError('That file does not appear to be an audio file')
+            if (audio.content_type[0:6] != 'audio/' or
+                    ext not in ('.ogg', '.mp3', '.wav', '.3gp')):
+                raise forms.ValidationError(
+                    'That file does not appear to be an audio file')
         return audio
+
 
 class SpeechAudioForm(forms.ModelForm, CleanAudioMixin):
     class Meta:
         model = Speech
-        fields = ( 'audio', )
+        fields = ('audio',)
         widgets = {
             'audio': AudioFileInput,
         }
 
+
 class SectionPickForm(forms.Form):
-    section = forms.ModelChoiceField(label=_('Assign to section'), queryset=Section.objects.all(), required=True)
+    section = forms.ModelChoiceField(
+        label=_('Assign to section'),
+        queryset=Section.objects.all(),
+        required=True,
+        )
+
 
 class Select2Widget(AutoHeavySelect2Widget):
     def __init__(self, *args, **kwargs):
-        # AutoHeavySelect2Mixin's __init__ hard codes 'data_view' without giving
-        # us the chance to add in the namespace we need. Let's hard code it
-        # ourselves and then skip past AutoHeavySelect2Mixin in the MRO
-        # to HeavySelect2Widget and continue from there.
+        # AutoHeavySelect2Mixin's __init__ hard codes 'data_view' without
+        # giving us the chance to add in the namespace we need. Let's
+        # hard code it ourselves and then skip past
+        # AutoHeavySelect2Mixin in the MRO to HeavySelect2Widget and
+        # continue from there.
         kwargs['data_view'] = "speeches:django_select2_central_json"
 
         HeavySelect2Widget.__init__(self, *args, **kwargs)
 
     def init_options(self):
         super(Select2Widget, self).init_options()
-        self.options['createSearchChoice'] = JSFunctionInContext('django_select2.createSearchChoice')
+        self.options['createSearchChoice'] = JSFunctionInContext(
+            'django_select2.createSearchChoice')
 
     def value_from_datadict(self, data, files, name):
         # Inspiration from MultipleSelect2HiddenInput
@@ -104,6 +114,7 @@ class Select2Widget(AutoHeavySelect2Widget):
             value = value[0] if len(value) else None
         return super(Select2Widget, self).render(name, value, attrs, choices)
 
+
 class StripWhitespaceField(forms.CharField):
     def clean(self, value):
         value = super(StripWhitespaceField, self).clean(value)
@@ -111,8 +122,9 @@ class StripWhitespaceField(forms.CharField):
             value = value.strip()
         return value
 
+
 class CreateAutoModelSelect2Field(AutoModelSelect2Field):
-    empty_values = [ None, '', [], (), {} ]
+    empty_values = [None, '', [], (), {}]
 
     # If anything tries to run a query on .queryset, it means we've missed
     # somewhere where we needed to limit things to the instance
@@ -123,13 +135,16 @@ class CreateAutoModelSelect2Field(AutoModelSelect2Field):
     instance = None
 
     def __init__(self, *args, **kwargs):
-        self.search_fields = [ '%s__icontains' % self.column ]
+        self.search_fields = ['%s__icontains' % self.column]
         if 'widget' in kwargs or 'empty_label' in kwargs:
-            logger.warn('widget/empty_label will be overwritten by using this field')
-        kwargs['widget'] = Select2Widget(select2_options={ 'placeholder': ' ', 'width': '100%' })
+            logger.warn(
+                'widget/empty_label will be overwritten by using this field')
+        kwargs['widget'] = Select2Widget(
+            select2_options={'placeholder': ' ', 'width': '100%'})
         kwargs['empty_label'] = ''
         kwargs['required'] = kwargs.get('required', False)
-        return super(CreateAutoModelSelect2Field, self).__init__(*args, **kwargs)
+        return (super(CreateAutoModelSelect2Field, self)
+                .__init__(*args, **kwargs))
 
     def to_python(self, value):
         value = value.strip()
@@ -142,9 +157,12 @@ class CreateAutoModelSelect2Field(AutoModelSelect2Field):
             value = self.queryset.get(**{key: value})
         except (ValueError, self.model.DoesNotExist):
             # Note that value could currently arrive as two different things:
-            # a stringified integer representing the id of an existing object, or
-            # any other string representing the representative string of the object.
-            value = self.queryset.create(**{ self.column: value, 'instance': self.instance })
+            # 1) a stringified integer representing the id of an existing
+            #    object, or
+            # 2) any other string representing the representative string of
+            #    the object.
+            value = self.queryset.create(
+                **{self.column: value, 'instance': self.instance})
         return value
 
     def clean(self, value):
@@ -166,13 +184,16 @@ class CreateAutoModelSelect2Field(AutoModelSelect2Field):
 
         return value
 
+
 class SpeakerField(CreateAutoModelSelect2Field):
     model = Speaker
     column = 'name'
 
+
 class SectionField(CreateAutoModelSelect2Field):
     model = Section
     column = 'heading'
+
 
 class SpeechTextFieldWidget(forms.Textarea):
     def render(self, name, value, attrs=None):
@@ -186,8 +207,10 @@ class SpeechTextFieldWidget(forms.Textarea):
 
         return super(SpeechTextFieldWidget, self).render(name, value, attrs)
 
+
 class SpeechTextField(StripWhitespaceField):
     widget = SpeechTextFieldWidget
+
     def clean(self, value):
         value = super(SpeechTextField, self).clean(value)
 
@@ -199,6 +222,7 @@ class SpeechTextField(StripWhitespaceField):
             else:
                 value = linebreaks(value)
         return value
+
 
 class SpeechForm(forms.ModelForm, CleanAudioMixin):
     audio_filename = forms.CharField(widget=forms.HiddenInput, required=False)
@@ -237,10 +261,14 @@ class SpeechForm(forms.ModelForm, CleanAudioMixin):
         required=False,
         )
     #tags = TagField()
-    tags = forms.ModelMultipleChoiceField(queryset=Tag.objects.all(),
-            label=verbose_name(Speech, 'tags'),
-            widget=Select2MultipleWidget(select2_options={ 'placeholder':_('Choose tags'), 'width': 'resolve' }),
-            required=False)
+    tags = forms.ModelMultipleChoiceField(
+        queryset=Tag.objects.all(),
+        label=verbose_name(Speech, 'tags'),
+        widget=Select2MultipleWidget(
+            select2_options={'placeholder': _('Choose tags'),
+                             'width': 'resolve'}),
+        required=False,
+        )
 
     def clean(self):
         cleaned_data = self.cleaned_data
@@ -255,11 +283,13 @@ class SpeechForm(forms.ModelForm, CleanAudioMixin):
             self.cleaned_data['audio'] = filename
 
         if not cleaned_data.get('text') and not cleaned_data.get('audio'):
-            raise forms.ValidationError(_('You must provide either text or some audio'))
+            raise forms.ValidationError(
+                _('You must provide either text or some audio'))
 
-        # If we have text but no speaker, then this should become a <narrative> element
-        # in the Akoma Ntoso, which can't contain <p> elements, so we should replace any
-        # in the middle with <br /> and get rid of the ones round the outside.
+        # If we have text but no speaker, then this should become a
+        # <narrative> element in the Akoma Ntoso, which can't contain
+        # <p> elements, so we should replace any in the middle with
+        # <br /> and get rid of the ones round the outside.
         if 'text' in cleaned_data and not cleaned_data.get('speaker'):
             text = cleaned_data['text']
             text = re.sub(r'</p>\n\n<p>', '<br />\n', text)
@@ -269,13 +299,17 @@ class SpeechForm(forms.ModelForm, CleanAudioMixin):
         return cleaned_data
 
     def clean_start_time(self):
-        if self.cleaned_data['start_time'] and not self.cleaned_data.get('start_date'):
-            raise forms.ValidationError(_('If you provide a start time you must give a start date too'))
+        if (self.cleaned_data['start_time'] and
+                not self.cleaned_data.get('start_date')):
+            raise forms.ValidationError(
+                _('If you provide a start time you must give a start date too')
+                )
         return self.cleaned_data['start_time']
 
     def clean_end_time(self):
         if self.cleaned_data['end_time'] and not self.cleaned_data['end_date']:
-            raise forms.ValidationError(_('If you provide an end time you must give an end date too'))
+            raise forms.ValidationError(
+                _('If you provide an end time you must give an end date too'))
         return self.cleaned_data['end_time']
 
     class Meta:
@@ -288,6 +322,7 @@ class SpeechForm(forms.ModelForm, CleanAudioMixin):
             'source_url': forms.TextInput(),
         }
         exclude = ('celery_task_id', 'instance')
+
 
 class RecordingAPIForm(forms.ModelForm, CleanAudioMixin):
     # Form for uploading a recording
@@ -307,15 +342,18 @@ class RecordingAPIForm(forms.ModelForm, CleanAudioMixin):
         return cleaned_data
 
     def clean_timestamps(self):
-        # Allow uploading of timestamps which don't exist by turning the supplied
-        # dictionary of timestamps into new RecordingTimestamp instances
+        # Allow uploading of timestamps which don't exist by turning
+        # the supplied dictionary of timestamps into new
+        # RecordingTimestamp instances
         timestamps = []
 
-        if not 'timestamps' in self.cleaned_data or not self.cleaned_data['timestamps']:
+        if ('timestamps' not in self.cleaned_data or
+                not self.cleaned_data['timestamps']):
             logger.debug('No timestamps in cleaned_data')
             return timestamps
 
-        logger.debug('timestamps received = %s' % self.cleaned_data['timestamps'])
+        logger.debug(
+            'timestamps received = %s' % self.cleaned_data['timestamps'])
         timestamps_json = json.loads(self.cleaned_data['timestamps'])
 
         if not isinstance(timestamps_json, list):
@@ -324,27 +362,42 @@ class RecordingAPIForm(forms.ModelForm, CleanAudioMixin):
         for recording_timestamp in timestamps_json:
             try:
                 if 'timestamp' in recording_timestamp:
-                    # Note - we divide by 1000 because the time comes from javascript
-                    # and is in milliseconds, but this expects the time in seconds
+                    # Note - we divide by 1000 because the time comes
+                    # from javascript and is in milliseconds, but this
+                    # expects the time in seconds
                     supplied_time = int(recording_timestamp['timestamp']/1000)
                     # We also make it a UTC time!
-                    timestamp = datetime.utcfromtimestamp(supplied_time).replace(tzinfo=pytz.utc)
+                    timestamp = (datetime.utcfromtimestamp(supplied_time)
+                                 .replace(tzinfo=pytz.utc))
                     try:
-                        speaker = Speaker.objects.get(pk=recording_timestamp['speaker'])
+                        speaker = Speaker.objects.get(
+                            pk=recording_timestamp['speaker'])
                     except:
                         speaker = None
-                    timestamps.append(RecordingTimestamp(speaker=speaker, timestamp=timestamp, instance=self.request.instance))
+                    timestamps.append(
+                        RecordingTimestamp(
+                            speaker=speaker,
+                            timestamp=timestamp,
+                            instance=self.request.instance)
+                        )
                 else:
                     # Timestamp is required
-                    logger.error("No timestamp supplied in request: {0}".format(recording_timestamp))
-                    pass
+                    logger.error(
+                        "No timestamp supplied in request: {0}".format(
+                            recording_timestamp)
+                        )
             except ValueError:
                 # Ignore this one
-                logger.error("ValueError encountered parsing: {0} into speaker/timestamp".format(recording_timestamp))
-
+                logger.error(
+                    "ValueError encountered parsing: {0} into speaker/timestamp"
+                    .format(recording_timestamp)
+                    )
 
         if timestamps.count == 0:
-            logger.error("Timestamps parameter was given but no timestamps parsed from: {0}".format(self.cleaned_data['timestamp']))
+            logger.error(
+                "Timestamps parameter was given but no timestamps parsed from: {0}"
+                .format(self.cleaned_data['timestamp'])
+                )
 
         return timestamps
 
@@ -352,16 +405,24 @@ class RecordingAPIForm(forms.ModelForm, CleanAudioMixin):
         model = Recording
         exclude = ('instance',)
 
+
 class RecordingForm(forms.ModelForm):
     class Meta:
         model = Recording
         exclude = ['instance', 'audio']
 
-class SectionForm(forms.ModelForm):
-    heading = forms.CharField(required=True, label=verbose_name(Section, 'heading'))
-    parent = GroupedModelChoiceField(Section.objects.all(), 'parent',
-        label=verbose_name(Section, 'parent'), required=False)
 
+class SectionForm(forms.ModelForm):
+    heading = forms.CharField(
+        required=True,
+        label=verbose_name(Section, 'heading'),
+        )
+    parent = GroupedModelChoiceField(
+        Section.objects.all(),
+        'parent',
+        label=verbose_name(Section, 'parent'),
+        required=False,
+        )
     start_date = forms.DateField(
         label=verbose_name(Section, 'start_date'),
         widget=DatePickerWidget,
@@ -378,8 +439,8 @@ class SectionForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super(SectionForm, self).__init__(*args, **kwargs)
         if self.instance.id:
-            ids = [ self.instance.id ]
-            ids.extend( [ d.id for d in self.instance.get_descendants ] )
+            ids = [self.instance.id]
+            ids.extend([d.id for d in self.instance.get_descendants])
             self.fields['parent'].queryset = Section.objects.exclude(id__in=ids)
 
     class Meta:
@@ -390,11 +451,15 @@ class SectionForm(forms.ModelForm):
         parent = self.cleaned_data['parent']
         if self.instance and parent:
             if parent.id == self.instance.id:
-                raise forms.ValidationError(_('Something cannot be its own parent'))
-            descendant_ids = [ d.id for d in self.instance.get_descendants ]
+                raise forms.ValidationError(
+                    _('Something cannot be its own parent'))
+            descendant_ids = [d.id for d in self.instance.get_descendants]
             if parent.id in descendant_ids:
-                raise forms.ValidationError(_('Something cannot have a parent that is also a descendant'))
+                raise forms.ValidationError(
+                    _('Something cannot have a parent that is also a descendant')
+                    )
         return parent
+
 
 class SpeakerForm(forms.ModelForm):
     name = StripWhitespaceField(label=verbose_name(Speaker, 'name'))
@@ -402,6 +467,7 @@ class SpeakerForm(forms.ModelForm):
     class Meta:
         model = Speaker
         exclude = ('instance', 'slug')
+
 
 class RecordingTimestampForm(forms.ModelForm):
     timestamp = FromStartIntegerField()
@@ -413,14 +479,14 @@ class RecordingTimestampForm(forms.ModelForm):
         if self.instance.recording_id:
             self.fields['timestamp'].recording_start = self.instance.recording.start_datetime
 
-
     def save(self, commit=True):
         self.instance.instance_id = self.instance.recording.instance_id
         return super(RecordingTimestampForm, self).save(commit)
 
     class Meta:
         model = RecordingTimestamp
-        exclude = ['instance','speech']
+        exclude = ['instance', 'speech']
+
 
 class BaseRecordingTimestampFormSet(BaseInlineFormSet):
     def clean(self):
@@ -432,17 +498,18 @@ class BaseRecordingTimestampFormSet(BaseInlineFormSet):
         # we're using '_forms' to avoid clashing with forms import, e.g.
         # for ValidationError
         _forms = sorted(
-            [ f for f in self.forms if 'timestamp' in f.cleaned_data ],
-            key = lambda f: f.cleaned_data['timestamp']
+            [f for f in self.forms if 'timestamp' in f.cleaned_data],
+            key=lambda f: f.cleaned_data['timestamp']
         )
 
         first_timestamp = _forms[0].cleaned_data['timestamp']
-        last_timestamp =  _forms[-1].cleaned_data['timestamp']
+        last_timestamp = _forms[-1].cleaned_data['timestamp']
 
         # TODO: check that first timestamp isn't before start of speech?
 
         if first_timestamp < recording.start_datetime:
-            raise forms.ValidationError(_("Start time is before recording start time!"))
+            raise forms.ValidationError(
+                _("Start time is before recording start time!"))
 
         # TODO: check that delta from first to last timestamp isn't longer
         # than length of audio
@@ -451,21 +518,23 @@ class BaseRecordingTimestampFormSet(BaseInlineFormSet):
 
         delta = (last_timestamp - first_timestamp).seconds
         if delta >= recording.audio_duration:
-            raise forms.ValidationError(_('Difference between timestamps is too long for the uploaded audio'))
+            raise forms.ValidationError(
+                _('Difference between timestamps is too long for the uploaded audio'))
 
         previous_timestamp = None
         for form in _forms:
             timestamp = form.cleaned_data['timestamp']
             if previous_timestamp:
                 if timestamp <= previous_timestamp:
-                    raise forms.ValidationError(_('Timestamps must be distinct'))
+                    raise forms.ValidationError(
+                        _('Timestamps must be distinct'))
             previous_timestamp = timestamp
 
 RecordingTimestampFormSet = inlineformset_factory(
     Recording,
     RecordingTimestamp,
-    formset = BaseRecordingTimestampFormSet,
-    form = RecordingTimestampForm,
-    extra = 1,
-    can_delete = 1,
+    formset=BaseRecordingTimestampFormSet,
+    form=RecordingTimestampForm,
+    extra=1,
+    can_delete=1,
 )
