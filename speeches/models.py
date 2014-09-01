mysociety/sayit

commit 4a5c74efff83a71385a5aebad53f45f5c4014e5e

Add verbose names and app_labels to models.
So that these can be translated - see
https://github.com/mysociety/sayit.mysociety.org/issues/52

speeches/models.py
@@ -83,6 +83,9 @@ class Speaker(InstanceMixin, Person):
     slugs = generic.GenericRelation(Slug)
 
     class Meta:
+        app_label = _('Speeches')
+        verbose_name = _('speaker')
+        verbose_name_plural = _('speakers')
         ordering = ('name',)
         unique_together = ('instance', 'slug')
 
@@ -108,6 +111,11 @@ class Tag(InstanceMixin, AuditedModel):
 
     def __str__(self):
         return self.name
+
+    class Meta:
+        app_label = _('Speeches')
+        verbose_name = _('tag')
+        verbose_name_plural = _('tags')
 
 
 class SectionManager(InstanceManager, Manager):
@@ -163,6 +171,9 @@ class Section(AuditedModel, InstanceMixin):
     slugs = generic.GenericRelation(Slug)
 
     class Meta:
+        app_label = _('Speeches')
+        verbose_name = _('section')
+        verbose_name_plural = _('sections')
         ordering = ('id',)
         unique_together = ('parent', 'slug', 'instance')
 
@@ -561,7 +572,9 @@ class Speech(InstanceMixin, AudioMP3Mixin, AuditedModel):
         _('celery task ID'), max_length=256, null=True, blank=True)
 
     class Meta:
-        verbose_name_plural = 'speeches'
+        app_label = _('Speeches')
+        verbose_name = _('speech')
+        verbose_name_plural = _('speeches')
         ordering = ( 'start_date', 'start_time', 'id' )
 
     def __str__(self):

speeches/locale/en/LC_MESSAGES/django.po
@@ -8,7 +8,7 @@ msgid ""
 msgstr ""
 "Project-Id-Version: PACKAGE VERSION\n"
 "Report-Msgid-Bugs-To: \n"
-"POT-Creation-Date: 2014-08-26 16:51+0100\n"
+"POT-Creation-Date: 2014-08-29 21:24+0100\n"
 "PO-Revision-Date: YEAR-MO-DA HO:MI+ZONE\n"
 "Last-Translator: FULL NAME <EMAIL@ADDRESS>\n"
 "Language-Team: LANGUAGE <LL@li.org>\n"
@@ -57,216 +57,253 @@ msgstr ""
 msgid "Timestamps must be distinct"
 msgstr ""
 
-#: models.py:82 models.py:160
+#: models.py:82 models.py:168
 msgid "slug"
 msgstr ""
 
-#: models.py:138 models.py:496
-msgid "number"
+#: models.py:86 models.py:116 models.py:174 models.py:575
+#: templates/speeches/base.html:27 templates/speeches/parentless_list.html:7
+#: templates/speeches/parentless_list.html:23
+#: templates/speeches/speaker_detail.html:90
+msgid "Speeches"
 msgstr ""
 
-#: models.py:138
-msgid "The number of the section"
-msgstr ""
-
-#: models.py:140 models.py:499
-msgid "heading"
-msgstr ""
-
-#: models.py:140
-msgid "The heading of the section"
-msgstr ""
-
-#: models.py:142 models.py:502
-msgid "subheading"
-msgstr ""
-
-#: models.py:143
-msgid "The subheading of the section"
-msgstr ""
-
-#: models.py:145
-msgid "description"
-msgstr ""
-
-#: models.py:145
-msgid "Longer description, HTML"
-msgstr ""
-
-#: models.py:147 models.py:535 extra-locales/popolo/behaviors/models.py:51
-msgid "start date"
-msgstr ""
-
-#: models.py:148
-msgid "What date did the section start?"
-msgstr ""
-
-#: models.py:150 models.py:538
-msgid "start time"
-msgstr ""
-
-#: models.py:151
-msgid "What time did the section start?"
-msgstr ""
-
-#: models.py:152
-msgid "document number"
-msgstr ""
-
-#: models.py:153
-msgid "legislature"
-msgstr ""
-
-#: models.py:155
-msgid "session"
-msgstr ""
-
-#: models.py:155
-msgid "Legislative session"
-msgstr ""
-
-#: models.py:157
-msgid "parent"
-msgstr ""
-
-#: models.py:161 models.py:556
-msgid "source URL"
-msgstr ""
-
-#: models.py:170 models.py:492
-msgid "Section"
-msgstr ""
-
-#: models.py:180
-msgid "You must specify at least one of num/heading/subheading"
-msgstr ""
-
-#: models.py:485
-msgid "audio"
-msgstr ""
-
-#: models.py:487
-msgid "text"
-msgstr ""
-
-#: models.py:488
-msgid "The text of the speech"
-msgstr ""
-
-#: models.py:497
-msgid "The number of the speech, if relevant"
-msgstr ""
-
-#: models.py:500
-msgid "The heading of the speech, if relevant"
-msgstr ""
-
-#: models.py:503
-msgid "The subheading of the speech, if relevant"
-msgstr ""
-
-#: models.py:507
-msgid "event"
-msgstr ""
-
-#: models.py:508
-msgid "Was the speech at a particular event?"
-msgstr ""
-
-#: models.py:510
-msgid "location"
-msgstr ""
-
-#: models.py:511
-msgid "Where the speech took place"
-msgstr ""
-
-#: models.py:515
+#: models.py:87 models.py:526
 msgid "speaker"
 msgstr ""
 
-#: models.py:516
+#: models.py:88
+msgid "speakers"
+msgstr ""
+
+#: models.py:117
+msgid "tag"
+msgstr ""
+
+#: models.py:118 models.py:558
+msgid "tags"
+msgstr ""
+
+#: models.py:146 models.py:507
+msgid "number"
+msgstr ""
+
+#: models.py:146
+msgid "The number of the section"
+msgstr ""
+
+#: models.py:148 models.py:510
+msgid "heading"
+msgstr ""
+
+#: models.py:148
+msgid "The heading of the section"
+msgstr ""
+
+#: models.py:150 models.py:513
+msgid "subheading"
+msgstr ""
+
+#: models.py:151
+msgid "The subheading of the section"
+msgstr ""
+
+#: models.py:153
+msgid "description"
+msgstr ""
+
+#: models.py:153
+msgid "Longer description, HTML"
+msgstr ""
+
+#: models.py:155 models.py:546 extra-locales/popolo/behaviors/models.py:51
+msgid "start date"
+msgstr ""
+
+#: models.py:156
+msgid "What date did the section start?"
+msgstr ""
+
+#: models.py:158 models.py:549
+msgid "start time"
+msgstr ""
+
+#: models.py:159
+msgid "What time did the section start?"
+msgstr ""
+
+#: models.py:160
+msgid "document number"
+msgstr ""
+
+#: models.py:161
+msgid "legislature"
+msgstr ""
+
+#: models.py:163
+msgid "session"
+msgstr ""
+
+#: models.py:163
+msgid "Legislative session"
+msgstr ""
+
+#: models.py:165
+msgid "parent"
+msgstr ""
+
+#: models.py:169 models.py:567
+msgid "source URL"
+msgstr ""
+
+#: models.py:175
+msgid "section"
+msgstr ""
+
+#: models.py:176
+msgid "sections"
+msgstr ""
+
+#: models.py:181 models.py:503
+msgid "Section"
+msgstr ""
+
+#: models.py:191
+msgid "You must specify at least one of num/heading/subheading"
+msgstr ""
+
+#: models.py:496
+msgid "audio"
+msgstr ""
+
+#: models.py:498
+msgid "text"
+msgstr ""
+
+#: models.py:499
+msgid "The text of the speech"
+msgstr ""
+
+#: models.py:508
+msgid "The number of the speech, if relevant"
+msgstr ""
+
+#: models.py:511
+msgid "The heading of the speech, if relevant"
+msgstr ""
+
+#: models.py:514
+msgid "The subheading of the speech, if relevant"
+msgstr ""
+
+#: models.py:518
+msgid "event"
+msgstr ""
+
+#: models.py:519
+msgid "Was the speech at a particular event?"
+msgstr ""
+
+#: models.py:521
+msgid "location"
+msgstr ""
+
+#: models.py:522
+msgid "Where the speech took place"
+msgstr ""
+
+#: models.py:527
 msgid "Who gave this speech?"
 msgstr ""
 
-#: models.py:521 extra-locales/popolo/models.py:233
+#: models.py:532 extra-locales/popolo/models.py:233
 msgid "type"
 msgstr ""
 
-#: models.py:523 templates/speeches/speaker_detail.html:51
+#: models.py:534 templates/speeches/speaker_detail.html:51
 #, fuzzy
 msgid "Speech"
 msgstr ""
 "#-#-#-#-#  django.pot (PACKAGE VERSION)  #-#-#-#-#\n"
 "#-#-#-#-#  django.pot (PACKAGE VERSION)  #-#-#-#-#\n"
 
-#: models.py:524
+#: models.py:535
 msgid "Question"
 msgstr ""
 
-#: models.py:525
+#: models.py:536
 msgid "Answer"
 msgstr ""
 
-#: models.py:526
+#: models.py:537
 msgid "Scene"
 msgstr ""
 
-#: models.py:527
+#: models.py:538
 msgid "Narrative"
 msgstr ""
 
-#: models.py:528
+#: models.py:539
 msgid "Summary"
 msgstr ""
 
-#: models.py:529
+#: models.py:540
 msgid "Other"
 msgstr ""
 
-#: models.py:531
+#: models.py:542
 msgid "What sort of speech is this?"
 msgstr ""
 
-#: models.py:536
+#: models.py:547
 msgid "What date did the speech start?"
 msgstr ""
 
-#: models.py:539
+#: models.py:550
 msgid "What time did the speech start?"
 msgstr ""
 
-#: models.py:541 extra-locales/popolo/behaviors/models.py:56
+#: models.py:552 extra-locales/popolo/behaviors/models.py:56
 msgid "end date"
 msgstr ""
 
-#: models.py:542
+#: models.py:553
 msgid "What date did the speech end?"
 msgstr ""
 
-#: models.py:544
+#: models.py:555
 msgid "end time"
 msgstr ""
 
-#: models.py:545
+#: models.py:556
 msgid "What time did the speech end?"
 msgstr ""
 
-#: models.py:547
-msgid "tags"
-msgstr ""
-
-#: models.py:550
+#: models.py:561
 msgid "public"
 msgstr ""
 
-#: models.py:550
+#: models.py:561
 msgid "Is this speech public?"
 msgstr ""
 
-#: models.py:561
+#: models.py:572
 msgid "celery task ID"
 msgstr ""
+
+#: models.py:576
+#, fuzzy
+msgid "speech"
+msgstr ""
+"#-#-#-#-#  django.pot (PACKAGE VERSION)  #-#-#-#-#\n"
+"#-#-#-#-#  django.pot (PACKAGE VERSION)  #-#-#-#-#\n"
+
+#: models.py:577
+#, fuzzy
+msgid "speeches"
+msgstr ""
+"#-#-#-#-#  django.pot (PACKAGE VERSION)  #-#-#-#-#\n"
+"#-#-#-#-#  django.pot (PACKAGE VERSION)  #-#-#-#-#\n"
 
 #: views.py:409
 msgid "Speeches assigned."
@@ -715,12 +752,6 @@ msgstr ""
 msgid "Home"
 msgstr ""
 
-#: templates/speeches/base.html:27 templates/speeches/parentless_list.html:7
-#: templates/speeches/parentless_list.html:23
-#: templates/speeches/speaker_detail.html:90
-msgid "Speeches"
-msgstr ""
-
 #: templates/speeches/parentless_list.html:20
 msgid "Add a new speech"
 msgstr ""
@@ -890,24 +921,26 @@ msgstr ""
 msgid "Invisible"
 msgstr ""
 
-#: templates/speeches/speech.html:101
-msgid "[ recorded audio ]"
-msgstr ""
-
-#: templates/speeches/speech.html:105 templates/speeches/speech.html.py:126
+#: templates/speeches/speech.html:97 templates/speeches/speech.html.py:113
+#: templates/speeches/speech.html:138
 msgid "Link in context"
 msgstr ""
 
-#: templates/speeches/speech.html:107 templates/speeches/speech.html.py:128
+#: templates/speeches/speech.html:99 templates/speeches/speech.html.py:115
+#: templates/speeches/speech.html:140
 msgid "Link"
 msgstr ""
 
-#: templates/speeches/speech.html:122
+#: templates/speeches/speech.html:109
+msgid "[ recorded audio ]"
+msgstr ""
+
+#: templates/speeches/speech.html:134
 #: templates/speeches/speech_detail.html:112
 msgid "Edit"
 msgstr ""
 
-#: templates/speeches/speech.html:123
+#: templates/speeches/speech.html:135
 #: templates/speeches/speech_detail.html:113
 msgid "Delete"
 msgstr ""

speeches/locale/es/LC_MESSAGES/django.po
@@ -9,7 +9,7 @@ msgid ""
 msgstr ""
 "Project-Id-Version: sayit\n"
 "Report-Msgid-Bugs-To: \n"
-"POT-Creation-Date: 2014-08-26 16:51+0100\n"
+"POT-Creation-Date: 2014-08-29 21:24+0100\n"
 "PO-Revision-Date: 2014-07-31 16:50+0000\n"
 "Last-Translator: johnfelipe <ingenierofelipeurrego@gmail.com>\n"
 "Language-Team: Spanish (http://www.transifex.com/projects/p/sayit/language/"
@@ -65,221 +65,257 @@ msgstr ""
 msgid "Timestamps must be distinct"
 msgstr "Las marcas de tiempo deben ser distintas"
 
-#: models.py:82 models.py:160
+#: models.py:82 models.py:168
 msgid "slug"
 msgstr ""
 
-#: models.py:138 models.py:496
-msgid "number"
-msgstr ""
+#: models.py:86 models.py:116 models.py:174 models.py:575
+#: templates/speeches/base.html:27 templates/speeches/parentless_list.html:7
+#: templates/speeches/parentless_list.html:23
+#: templates/speeches/speaker_detail.html:90
+msgid "Speeches"
+msgstr "Actas"
 
-#: models.py:138
-#, fuzzy
-msgid "The number of the section"
-msgstr "El número de la sección"
-
-#: models.py:140 models.py:499
-msgid "heading"
-msgstr ""
-
-#: models.py:140
-msgid "The heading of the section"
-msgstr "El título de la sección"
-
-#: models.py:142 models.py:502
-msgid "subheading"
-msgstr ""
-
-#: models.py:143
-msgid "The subheading of the section"
-msgstr "El subtítulo de la sección"
-
-#: models.py:145
-#, fuzzy
-msgid "description"
-msgstr "sección"
-
-#: models.py:145
-msgid "Longer description, HTML"
-msgstr "Descripción más larga, HTML"
-
-#: models.py:147 models.py:535 extra-locales/popolo/behaviors/models.py:51
-msgid "start date"
-msgstr ""
-
-#: models.py:148
-msgid "What date did the section start?"
-msgstr "¿En qué fecha se inicia la sección?"
-
-#: models.py:150 models.py:538
-msgid "start time"
-msgstr ""
-
-#: models.py:151
-msgid "What time did the section start?"
-msgstr "¿A qué hora se inicia la sección?"
-
-#: models.py:152
-#, fuzzy
-msgid "document number"
-msgstr "número de documento"
-
-#: models.py:153
-#, fuzzy
-msgid "legislature"
-msgstr "legislatura"
-
-#: models.py:155
-msgid "session"
-msgstr ""
-
-#: models.py:155
-msgid "Legislative session"
-msgstr "sesión Legislativa"
-
-#: models.py:157
-msgid "parent"
-msgstr ""
-
-#: models.py:161 models.py:556
-#, fuzzy
-msgid "source URL"
-msgstr "fuente"
-
-#: models.py:170 models.py:492
-msgid "Section"
-msgstr "sección"
-
-#: models.py:180
-msgid "You must specify at least one of num/heading/subheading"
-msgstr "Debe especificar al menos una de num / título / subtítulo"
-
-#: models.py:485
-msgid "audio"
-msgstr ""
-
-#: models.py:487
-msgid "text"
-msgstr ""
-
-#: models.py:488
-msgid "The text of the speech"
-msgstr "El texto del discurso"
-
-#: models.py:497
-#, fuzzy
-msgid "The number of the speech, if relevant"
-msgstr "El número de la intervención, si procede"
-
-#: models.py:500
-msgid "The heading of the speech, if relevant"
-msgstr "El rubro de la intervención, si procede"
-
-#: models.py:503
-msgid "The subheading of the speech, if relevant"
-msgstr "El subtítulo de la intervención, si procede"
-
-#: models.py:507
-msgid "event"
-msgstr ""
-
-#: models.py:508
-msgid "Was the speech at a particular event?"
-msgstr "Fue el discurso en un evento en particular?"
-
-#: models.py:510
-#, fuzzy
-msgid "location"
-msgstr "sección"
-
-#: models.py:511
-msgid "Where the speech took place"
-msgstr "Donde el discurso tuvo lugar"
-
-#: models.py:515
+#: models.py:87 models.py:526
 #, fuzzy
 msgid "speaker"
 msgstr "ponente"
 
-#: models.py:516
-msgid "Who gave this speech?"
-msgstr "¿Quién dio este discurso?"
+#: models.py:88
+#, fuzzy
+msgid "speakers"
+msgstr "ponente"
 
-#: models.py:521 extra-locales/popolo/models.py:233
-msgid "type"
+#: models.py:117
+msgid "tag"
 msgstr ""
 
-#: models.py:523 templates/speeches/speaker_detail.html:51
-msgid "Speech"
-msgstr "Discurso / Acta"
-
-#: models.py:524
-msgid "Question"
-msgstr "pregunta"
-
-#: models.py:525
-msgid "Answer"
-msgstr "Respuesta"
-
-#: models.py:526
-msgid "Scene"
-msgstr "Escena"
-
-#: models.py:527
-msgid "Narrative"
-msgstr "narrativa"
-
-#: models.py:528
-msgid "Summary"
-msgstr "resumen"
-
-#: models.py:529
-msgid "Other"
-msgstr "otro"
-
-#: models.py:531
-msgid "What sort of speech is this?"
-msgstr "¿Qué clase de palabra es ésta?"
-
-#: models.py:536
-msgid "What date did the speech start?"
-msgstr "¿En qué fecha se inicia el discurso?"
-
-#: models.py:539
-msgid "What time did the speech start?"
-msgstr "¿A qué hora empezó el discurso?"
-
-#: models.py:541 extra-locales/popolo/behaviors/models.py:56
-msgid "end date"
-msgstr ""
-
-#: models.py:542
-msgid "What date did the speech end?"
-msgstr "¿En qué fecha terminó el discurso?"
-
-#: models.py:544
-msgid "end time"
-msgstr ""
-
-#: models.py:545
-msgid "What time did the speech end?"
-msgstr "¿A qué hora terminó el discurso?"
-
-#: models.py:547
+#: models.py:118 models.py:558
 msgid "tags"
 msgstr ""
 
+#: models.py:146 models.py:507
+msgid "number"
+msgstr ""
+
+#: models.py:146
+#, fuzzy
+msgid "The number of the section"
+msgstr "El número de la sección"
+
+#: models.py:148 models.py:510
+msgid "heading"
+msgstr ""
+
+#: models.py:148
+msgid "The heading of the section"
+msgstr "El título de la sección"
+
+#: models.py:150 models.py:513
+msgid "subheading"
+msgstr ""
+
+#: models.py:151
+msgid "The subheading of the section"
+msgstr "El subtítulo de la sección"
+
+#: models.py:153
+#, fuzzy
+msgid "description"
+msgstr "sección"
+
+#: models.py:153
+msgid "Longer description, HTML"
+msgstr "Descripción más larga, HTML"
+
+#: models.py:155 models.py:546 extra-locales/popolo/behaviors/models.py:51
+msgid "start date"
+msgstr ""
+
+#: models.py:156
+msgid "What date did the section start?"
+msgstr "¿En qué fecha se inicia la sección?"
+
+#: models.py:158 models.py:549
+msgid "start time"
+msgstr ""
+
+#: models.py:159
+msgid "What time did the section start?"
+msgstr "¿A qué hora se inicia la sección?"
+
+#: models.py:160
+#, fuzzy
+msgid "document number"
+msgstr "número de documento"
+
+#: models.py:161
+#, fuzzy
+msgid "legislature"
+msgstr "legislatura"
+
+#: models.py:163
+msgid "session"
+msgstr ""
+
+#: models.py:163
+msgid "Legislative session"
+msgstr "sesión Legislativa"
+
+#: models.py:165
+msgid "parent"
+msgstr ""
+
+#: models.py:169 models.py:567
+#, fuzzy
+msgid "source URL"
+msgstr "fuente"
+
+#: models.py:175
+#, fuzzy
+msgid "section"
+msgstr "sección"
+
+#: models.py:176
+#, fuzzy
+msgid "sections"
+msgstr "sección"
+
+#: models.py:181 models.py:503
+msgid "Section"
+msgstr "sección"
+
+#: models.py:191
+msgid "You must specify at least one of num/heading/subheading"
+msgstr "Debe especificar al menos una de num / título / subtítulo"
+
+#: models.py:496
+msgid "audio"
+msgstr ""
+
+#: models.py:498
+msgid "text"
+msgstr ""
+
+#: models.py:499
+msgid "The text of the speech"
+msgstr "El texto del discurso"
+
+#: models.py:508
+#, fuzzy
+msgid "The number of the speech, if relevant"
+msgstr "El número de la intervención, si procede"
+
+#: models.py:511
+msgid "The heading of the speech, if relevant"
+msgstr "El rubro de la intervención, si procede"
+
+#: models.py:514
+msgid "The subheading of the speech, if relevant"
+msgstr "El subtítulo de la intervención, si procede"
+
+#: models.py:518
+msgid "event"
+msgstr ""
+
+#: models.py:519
+msgid "Was the speech at a particular event?"
+msgstr "Fue el discurso en un evento en particular?"
+
+#: models.py:521
+#, fuzzy
+msgid "location"
+msgstr "sección"
+
+#: models.py:522
+msgid "Where the speech took place"
+msgstr "Donde el discurso tuvo lugar"
+
+#: models.py:527
+msgid "Who gave this speech?"
+msgstr "¿Quién dio este discurso?"
+
+#: models.py:532 extra-locales/popolo/models.py:233
+msgid "type"
+msgstr ""
+
+#: models.py:534 templates/speeches/speaker_detail.html:51
+msgid "Speech"
+msgstr "Discurso / Acta"
+
+#: models.py:535
+msgid "Question"
+msgstr "pregunta"
+
+#: models.py:536
+msgid "Answer"
+msgstr "Respuesta"
+
+#: models.py:537
+msgid "Scene"
+msgstr "Escena"
+
+#: models.py:538
+msgid "Narrative"
+msgstr "narrativa"
+
+#: models.py:539
+msgid "Summary"
+msgstr "resumen"
+
+#: models.py:540
+msgid "Other"
+msgstr "otro"
+
+#: models.py:542
+msgid "What sort of speech is this?"
+msgstr "¿Qué clase de palabra es ésta?"
+
+#: models.py:547
+msgid "What date did the speech start?"
+msgstr "¿En qué fecha se inicia el discurso?"
+
 #: models.py:550
+msgid "What time did the speech start?"
+msgstr "¿A qué hora empezó el discurso?"
+
+#: models.py:552 extra-locales/popolo/behaviors/models.py:56
+msgid "end date"
+msgstr ""
+
+#: models.py:553
+msgid "What date did the speech end?"
+msgstr "¿En qué fecha terminó el discurso?"
+
+#: models.py:555
+msgid "end time"
+msgstr ""
+
+#: models.py:556
+msgid "What time did the speech end?"
+msgstr "¿A qué hora terminó el discurso?"
+
+#: models.py:561
 msgid "public"
 msgstr ""
 
-#: models.py:550
+#: models.py:561
 msgid "Is this speech public?"
 msgstr "Es pública este discurso?"
 
-#: models.py:561
+#: models.py:572
 msgid "celery task ID"
 msgstr ""
+
+#: models.py:576
+#, fuzzy
+msgid "speech"
+msgstr "Discurso / Acta"
+
+#: models.py:577
+#, fuzzy
+msgid "speeches"
+msgstr "Actas"
 
 #: views.py:409
 msgid "Speeches assigned."
@@ -612,8 +648,9 @@ msgid "The date when the validity of the item ends"
 msgstr ""
 
 #: extra-locales/popolo/behaviors/models.py:70
+#, fuzzy
 msgid "creation time"
-msgstr ""
+msgstr "sección"
 
 #: extra-locales/popolo/behaviors/models.py:71
 #, fuzzy
@@ -741,12 +778,6 @@ msgstr "Aprenda más sobre SayIt"
 #: templates/speeches/base.html:25
 msgid "Home"
 msgstr "Inicio"
-
-#: templates/speeches/base.html:27 templates/speeches/parentless_list.html:7
-#: templates/speeches/parentless_list.html:23
-#: templates/speeches/speaker_detail.html:90
-msgid "Speeches"
-msgstr "Actas"
 
 #: templates/speeches/parentless_list.html:20
 msgid "Add a new speech"
@@ -925,24 +956,26 @@ msgstr ""
 msgid "Invisible"
 msgstr "invisible"
 
-#: templates/speeches/speech.html:101
-msgid "[ recorded audio ]"
-msgstr "[Audio grabado]"
-
-#: templates/speeches/speech.html:105 templates/speeches/speech.html.py:126
+#: templates/speeches/speech.html:97 templates/speeches/speech.html.py:113
+#: templates/speeches/speech.html:138
 msgid "Link in context"
 msgstr "Enlace en contexto"
 
-#: templates/speeches/speech.html:107 templates/speeches/speech.html.py:128
+#: templates/speeches/speech.html:99 templates/speeches/speech.html.py:115
+#: templates/speeches/speech.html:140
 msgid "Link"
 msgstr "enlace"
 
-#: templates/speeches/speech.html:122
+#: templates/speeches/speech.html:109
+msgid "[ recorded audio ]"
+msgstr "[Audio grabado]"
+
+#: templates/speeches/speech.html:134
 #: templates/speeches/speech_detail.html:112
 msgid "Edit"
 msgstr "Editar"
 
-#: templates/speeches/speech.html:123
+#: templates/speeches/speech.html:135
 #: templates/speeches/speech_detail.html:113
 msgid "Delete"
 msgstr "Borrar"

commit 3d7e3695700b67f9ab5d236f2c8734cfbabe0ca3

app_label should not be a translated string.
Bug introduced by 4a5c74ef. Fixes #373.

speeches/models.py
@@ -83,7 +83,6 @@ class Speaker(InstanceMixin, Person):
     slugs = generic.GenericRelation(Slug)
 
     class Meta:
-        app_label = _('Speeches')
         verbose_name = _('speaker')
         verbose_name_plural = _('speakers')
         ordering = ('name',)
@@ -113,7 +112,6 @@ class Tag(InstanceMixin, AuditedModel):
         return self.name
 
     class Meta:
-        app_label = _('Speeches')
         verbose_name = _('tag')
         verbose_name_plural = _('tags')
 
@@ -171,7 +169,6 @@ class Section(AuditedModel, InstanceMixin):
     slugs = generic.GenericRelation(Slug)
 
     class Meta:
-        app_label = _('Speeches')
         verbose_name = _('section')
         verbose_name_plural = _('sections')
         ordering = ('id',)
@@ -572,7 +569,6 @@ class Speech(InstanceMixin, AudioMP3Mixin, AuditedModel):
         _('celery task ID'), max_length=256, null=True, blank=True)
 
     class Meta:
-        app_label = _('Speeches')
         verbose_name = _('speech')
         verbose_name_plural = _('speeches')
         ordering = ( 'start_date', 'start_time', 'id' )