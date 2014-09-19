78ddd8500881d6f54434290984742b892cdb42c0
Matthew Somerville <matthew-github@dracos.co.uk>
2014-09-19 16:36:03 +0100

    Version 1.2.

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ else:
 
 setup(
     name="django-sayit",
-    version='1.1.1',
+    version='1.2',
     description='A data store for speeches and transcripts to make them searchable and pretty.',
     long_description=read_file('README.rst'),
     author='mySociety',
