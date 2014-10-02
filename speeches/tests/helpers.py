08211226e731b13412410547008ac15c9e715bd0
Duncan Parkes <duncan@mysociety.org>
2014-10-03 00:09:55 +0100

    A bit of PEP8 for helpers.py

--- speeches/tests/helpers.py
+++ speeches/tests/helpers.py
@@ -1,7 +1,6 @@
-from datetime import datetime, date, time, timedelta
+from datetime import datetime, date, timedelta
 
 from django.test import TestCase, LiveServerTestCase
-from django.test.utils import override_settings
 from django.contrib.auth.models import User
 
 from selenium.webdriver.support.ui import WebDriverWait
@@ -10,6 +9,7 @@ from speeches.models import Section, Speech
 from instances.models import Instance
 
 from instances.tests import InstanceTestCase, InstanceLiveServerTestCase
+
 
 class InstanceTestCase(InstanceTestCase):
     default_instance_options = dict(label='default')
@@ -29,9 +29,9 @@ class ParentInstanceMismatchError(Exception):
     pass
 
 
-def create_sections( subsections, parent=None, instance=None):
+def create_sections(subsections, parent=None, instance=None):
     """
-    Create a hierachy of sections and speeches. Very useful for setting up test data as needed.
+    Create a hierachy of sections and speeches - good for setting up test data.
 
     Note - used in external packages (pombola.south_africa), alter with care.
 
@@ -58,23 +58,29 @@ def create_sections( subsections, parent=None, instance=None):
     ])
     """
 
-    # If given an instance and a parent check that they are compatible - we don't
-    # want to end up with a hierarchy that spans instances and this would probably
-    # mean that there is some confusion upstream that we should highlight.
+    # If given an instance and a parent check that they are compatible -
+    # we don't want to end up with a hierarchy that spans instances and
+    # this would probably mean that there is some confusion upstream that
+    # we should highlight.
     if instance and parent and instance != parent.instance:
-        raise ParentInstanceMismatchError("The instance and parent.instance do not match")
+        raise ParentInstanceMismatchError(
+            "The instance and parent.instance do not match")
 
     # If we have a parent then use the instance from that.
     if parent:
         instance = parent.instance
 
-    # If we don't have an instance (which also means we had no parent) then create
-    # one - this is very convenient in the test scripts.
+    # If we don't have an instance (which also means we had no parent)
+    # then create one - this is very convenient in the test scripts.
     if not instance:
         instance, _ = Instance.objects.get_or_create(label='create-sections-instance')
 
     for subsection in subsections:
-        s = Section.objects.create( instance=instance, heading=subsection['heading'], parent=parent )
+        s = Section.objects.create(
+                instance=instance,
+                heading=subsection['heading'],
+                parent=parent,
+                )
         if 'subsections' in subsection:
             create_sections(subsection['subsections'], parent=s)
         if 'speeches' in subsection:
@@ -88,13 +94,13 @@ def create_sections( subsections, parent=None, instance=None):
             num, d, t = speeches_details[:3]
             for i in range(0, num):
                 Speech.objects.create(
-                    instance = instance,
-                    section = s,
-                    text = 'rhubarb rhubarb',
-                    start_date = d,
-                    start_time = t,
-                    public = public,
-                    source_url = "http://somewhere.or.other/{0}".format(i)
+                    instance=instance,
+                    section=s,
+                    text='rhubarb rhubarb',
+                    start_date=d,
+                    start_time=t,
+                    public=public,
+                    source_url="http://somewhere.or.other/{0}".format(i),
                 )
                 if t:
                     t = (datetime.combine(date.today(), t) + timedelta(minutes=10)).time()
@@ -104,19 +110,22 @@ class CreateSectionsTests(TestCase):
     def test_parent_instance_mismatch(self):
         foo_instance = Instance.objects.create(label="foo")
         bar_instance = Instance.objects.create(label="bar")
-        foo_parent = Section.objects.create(instance=foo_instance, heading="Foo Section")
+        foo_parent = Section.objects.create(
+            instance=foo_instance,
+            heading="Foo Section",
+            )
 
         # Should run without exception
         create_sections([], parent=foo_parent, instance=foo_instance)
-        create_sections([],                    instance=foo_instance)
-        create_sections([], parent=foo_parent                      )
-        create_sections([],                                        )
+        create_sections([], instance=foo_instance)
+        create_sections([], parent=foo_parent)
+        create_sections([])
 
         # Should raise
         self.assertRaises(
             ParentInstanceMismatchError,
             create_sections,
-            subsections = [],
-            parent = foo_parent,
-            instance = bar_instance
+            subsections=[],
+            parent=foo_parent,
+            instance=bar_instance,
         )
